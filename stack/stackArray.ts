5436858b848679fc325fc1b79a0f1bf9f7ba2cf1
peter <peterkinfe548@gmail.com>
2025-05-29 21:46:43 +0300

    array implementation of a stack

---
+++ stack/stackArray.ts
@@ -0,0 +1,12 @@
+// let stack = []
+
+// stack.push("google")
+// stack.push("instagram")
+// stack.push("youtube")
+
+// stack.unshift("google")
+// stack.unshift("instagram")
+// stack.unshift("youtube")
+// in arrays pushing and poping are the best methods in implementing stacks
+// because there will not be shifting the index of the arrays but in shifting and unshifting
+// there will be shifting the index which is not suitable for a large datasets.
