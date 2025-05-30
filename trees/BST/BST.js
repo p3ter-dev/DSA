e40d07eebab5fc9d66afb144f2a0702c0f31c858
peter <peterkinfe548@gmail.com>
2025-05-30 11:12:56 +0300

    binary search tree

---
+++ trees/BST/BST.js
@@ -0,0 +1,19 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+}
+
+// let tree = new BinarySearchTree();
+// tree.root = new Node(10)
+// tree.root.left = new Node(9)
+// tree.root.right = new Node(15)
+// console.log(tree)
