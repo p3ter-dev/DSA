9544b3ab3879e681d51d9728161cbe1a6739cb02
peter <peterkinfe548@gmail.com>
2025-06-01 21:47:21 +0300

    dfs preorder search in binary trees

---
+++ trees/BST/DFS_preorder.js
@@ -0,0 +1,82 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+
+    insert(value) { //helps to insert value in a binary tree
+        let newNode = new Node(value)
+        if (this.root === null) {
+            this.root = newNode
+            return this
+        }
+        let current = this.root
+        while(true) {
+            if (value === current.value) return undefined
+            if (value < current.value) {
+                if (current.left === null) {
+                    current.left = newNode
+                    return this
+                }
+                current = current.left
+            } else if (value > current.value) {
+                if (current.right === null) {
+                    current.right = newNode
+                    return this
+                }
+                current = current.right
+            }
+        }
+    }
+
+    contains(value) { //helps us to find if a binary tree contains that value
+        let found = false
+        let current = this.root
+        if (current === null) return null
+
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left
+            } else if (value > current.value) {
+                current = current.right
+            } else {
+                return true
+            }
+        }
+        return false
+    }
+
+    BFS() { //breadth first search implementation
+        let data = [], 
+            queue = [], 
+            node = this.root;
+        queue.push(node);
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
+
+    preorder() { //depth first search in preorder implementation
+        let data = [];
+        let current = this.root;
+
+        function traverse(node) { //using recursion
+            data.push(node)
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+        traverse(current);
+        return data;
+    }
+}
