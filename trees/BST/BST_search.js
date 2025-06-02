87bc425a6c5a96ba9f3fc0d5727cf9ba7a101343
peter <peterkinfe548@gmail.com>
2025-06-02 23:43:36 +0300

    searching in binary search tree

--- trees/BST/BST_search.js
+++ trees/BST/BST_search.js
@@ -1,53 +1,53 @@
 class Node {
     constructor(value) {
-        this.value = value
-        this.left = null
-        this.right = null
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
 }
 
 class BinarySearchTree {
     constructor() {
-        this.root = null
+        this.root = null;
     }
     insert(value) { //helps to insert value in a binary tree
-        let newNode = new Node(value)
+        let newNode = new Node(value);
         if (this.root === null) {
-            this.root = newNode
-            return this
+            this.root = newNode;
+            return this;
         }
-        let current = this.root
+        let current = this.root;
         while(true) {
             if (value === current.value) return undefined
             if (value < current.value) {
                 if (current.left === null) {
-                    current.left = newNode
-                    return this
+                    current.left = newNode;
+                    return this;
                 }
-                current = current.left
+                current = current.left;
             } else if (value > current.value) {
                 if (current.right === null) {
-                    current.right = newNode
-                    return this
+                    current.right = newNode;
+                    return this;
                 }
-                current = current.right
+                current = current.right;
             }
         }
     }
     contains(value) { //helps us to find if a binary tree contains that value
-        let found = false
-        let current = this.root
+        let found = false;
+        let current = this.root;
         if (current === null) return null
 
         while (current && !found) {
             if (value < current.value) {
-                current = current.left
+                current = current.left;
             } else if (value > current.value) {
-                current = current.right
+                current = current.right;
             } else {
-                return true
+                return true;
             }
         }
-        return false
+        return false;
     }
 }
