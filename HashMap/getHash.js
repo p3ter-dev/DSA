a1aedbddbbeb138d1688c583d4d7468fb6901846
peter <peterkinfe548@gmail.com>
2025-06-03 23:09:22 +0300

    getting a key from a hashmap

---
+++ HashMap/getHash.js
@@ -0,0 +1,33 @@
+class HashTable {
+    constructor(size=53) {
+        this.keyMap = new Array(size);
+    }
+    _hash(key) {
+        let total = 0;
+        let PRIME_NUMBER = 31;
+        for (i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96;
+            total = (total * PRIME_NUMBER + value) % this.keyMap.length;
+        }
+        return total;
+    }
+
+    set(key, value) {
+        let index = this._hash(key);
+        if (!this.keyMap[index]) {
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, value]);
+    }
+
+    get(key) {
+        let index = this._hash(key);
+        if (this.keyMap[index]) {
+            for (i = 0; i < this.keyMap[index].length; i++) {
+                if (this.keyMap[index][i][0] === key) return this.keyMap[index][i][1];
+            }
+        }
+        return undefined;
+    }
+}
---
+++ HashMap/hashFunc.js
@@ -0,0 +1,12 @@
+function hash(key, arrayLength) { // simple hash
+    let total = 0;
+    let PRIME_NUMBER = 31;
+
+    for (i = 0; i < Math.min(key.length, 100); i++) {
+        // map "a" to 1, "b" to 2, and etc.
+        let char = key[i];
+        let value = char.charCodeAt(0) - 96;
+        total = (total * PRIME_NUMBER + value) % arrayLength;
+    }
+    return total;
+}
