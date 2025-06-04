0586b9d703b981923e45422f54c1bd696aa48b3e
peter <peterkinfe548@gmail.com>
2025-06-04 09:19:07 +0300

    insertion sort algorithm

---
+++ sorting/insertionSort.js
@@ -0,0 +1,10 @@
+function insertionSort(array) {
+    for(i = 1; i < array.length; i++) {
+        let currentValue = array[i];
+        for (j = i - 1; j > -1 && array[j] > currentValue; j--) {
+            array[j + 1] = array[j];
+        }
+        array[j + 1] = currentValue;
+    }
+    return array;
+}
