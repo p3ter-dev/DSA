8b4da715da71f8ddfcf919916e0dcd8a78060d55
peter <peterkinfe548@gmail.com>
2025-06-05 22:57:50 +0300

    binary search tree implemented in c++

---
+++ trees/BST/BST_insert.cpp
@@ -0,0 +1,51 @@
+class Node {
+public:
+    int value;
+    Node* left;
+    Node* right;
+    Node(int value) {
+        this->value = value;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+}
+
+class BinarySearchTree {
+public:
+    Node* root;
+    BinarySearchTree() {
+        this->root = nullptr;
+    }
+
+    bool insert(int value) {
+        Node* newNode = new Node(value);
+        if (!root) {
+            root = newNode;
+            return true;
+        }
+        Node* current = root;
+        while(true) {
+            if(value == current->value) return -1;
+            if (value < current->value) {
+                if (current->left == nullptr) {
+                    current->left = newNode;
+                    return this;
+                }
+                current = current->left;
+            } else if(value > current->value) {
+                if (current->right == nullptr) {
+                    current->right = newNode;
+                    return this;
+                }
+                current = current->value;
+            }
+        }
+    }
+
+    bool contains(int value) {
+        bool found = false;
+        Node* current = root;
+        
+
+    }
+}
