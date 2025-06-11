cabfb149dcf130c05999a5896a546b2c60e9aa42
peter <peterkinfe548@gmail.com>
2025-06-11 10:58:26 +0300

    hash function in c++

---
+++ HashMap/HashMap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class HashMap {
+public:
+    int size;
+    int keyMap;
+    HashMap(int size=53) {
+        this->keyMap = new Array(size);
+    }
+
+    void hash(key) {
+        int total = 0;
+        int PRIME_NUMBER = 31;
+        for (int i = 0; min(key.length, 100); i++) {
+            char character = key[i];
+            int value = character.length()
+        }
+    }
+}
