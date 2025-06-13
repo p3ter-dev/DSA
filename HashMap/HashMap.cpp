045cb047b68cdbb67bbdfa24ae3c9ea761b8437e
peter <peterkinfe548@gmail.com>
2025-06-13 16:41:01 +0300

    hash maps implemented in c++

--- HashMap/HashMap.cpp
+++ HashMap/HashMap.cpp
@@ -1,21 +1,46 @@
-#include <iostream>
-
+#include <vector>
+#include <list>
+#include <string>
 using namespace std;
 
-class HashMap {
-public:
+class HashTable {
+private:
+    vector<list<pair<string, string>>> keyMap;
     int size;
-    int keyMap;
-    HashMap(int size=53) {
-        this->keyMap = new Array(size);
-    }
 
-    void hash(key) {
+    int _hash(const string& key) {// hash function
         int total = 0;
         int PRIME_NUMBER = 31;
-        for (int i = 0; min(key.length, 100); i++) {
-            char character = key[i];
-            int value = character.length()
+        for (int i = 0; i < min((int)key.length(), 100); i++) {
+            char ch = key[i];
+            int value = tolower(ch) - 96;
+            total = (total * PRIME_NUMBER + value) % size;
         }
+        return total;
     }
-}
+
+public:
+    HashTable(int s = 53) {
+        size = s;
+        keyMap.resize(size);
+    }
+
+    void set(const string& key, const string& value) {
+        int index = _hash(key);
+        for (auto& pair : keyMap[index]) {
+            if (pair.first == key) {
+                pair.second = value;
+                return;
+            }
+        }
+        keyMap[index].push_back(make_pair(key, value));
+    }
+
+    string get(const string& key) {
+        int index = _hash(key);
+        for (const auto& pair : keyMap[index]) {
+            if (pair.first == key) return pair.second;
+        }
+        return "undefined";
+    }
+};
