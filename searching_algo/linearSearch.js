f4a346f1457e1bd8d399e393d2378e7873e454ec
peter <peterkinfe548@gmail.com>
2025-06-16 20:50:38 +0300

    linear search

--- searching_algo/linearSearch.js
+++ searching_algo/linearSearch.js
@@ -1,4 +1,4 @@
-function linearSearch(array, value) { //in linear search we visit one item at a time(unsorted array), by checking by
+const linearSearch = (array, value) => { //in linear search we visit one item at a time(unsorted array), by checking by
     for(i = 0; i < array.length; i++) {//starting from the beginning with O(n) time complexity on average
         if(array[i] === value) return i;//and worst case and O(1) best case.
     }
