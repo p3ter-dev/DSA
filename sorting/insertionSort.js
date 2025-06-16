090cf166138110b965eb337ca777ccd7912c685c
peter <peterkinfe548@gmail.com>
2025-06-16 20:29:28 +0300

    insertion sort

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,4 +1,4 @@
-function insertionSort(array) {
+const insertionSort = (array) => {
     for(i = 1; i < array.length; i++) {
         let currentValue = array[i];
         for (j = i - 1; j > -1 && array[j] > currentValue; j--) {
